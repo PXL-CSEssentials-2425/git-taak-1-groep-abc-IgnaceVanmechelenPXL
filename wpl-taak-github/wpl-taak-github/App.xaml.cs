7ca9231852a2352db657480bb765f532ca9db1da
IgnaceVanmechelenPXL <12402669@student.pxl.be>
2024-10-06 15:02:55 +0200

    KLEUR-01

---
+++ wpl-taak-github/wpl-taak-github/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpl_taak_github
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
